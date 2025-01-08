f37fdc45affc15bfeb74d90a502df25d94e2588f
Justin Angel <justinangel86@gmail.com>
2025-01-08 10:46:15 -0500

    testing charm tables

--- cmd/test_table.go
+++ cmd/test_table.go
@@ -7,20 +7,162 @@ import (
 	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/enescakir/emoji"
 	eavesarp_ng "github.com/impostorkeanu/eavesarp-ng"
 	_ "modernc.org/sqlite"
 	"os"
 )
 
+const (
+	mainTblQuery = `
+SELECT sender.id AS sender_ip_id,
+       sender.value AS sender_ip_value,
+       target.id AS target_ip_id,
+       target.value AS target_ip_value,
+       count AS arp_count,
+       (target.arp_resolved AND target.mac_id IS NULL) AS snac
+FROM arp_count
+    INNER JOIN ip AS sender ON arp_count.sender_ip_id = sender.id
+    INNER JOIN ip AS target ON arp_count.target_ip_id = target.id
+ORDER BY
+    snac DESC,
+    arp_count.count DESC`
+)
+
 type model struct {
 	table table.Model
+	db    *sql.DB
 }
 
-func (m model) Init() tea.Cmd { return nil }
+type TblContent struct {
+	cols []table.Column
+	rows []table.Row
+	err  error
+}
+
+func GetTableContent(db *sql.DB) (content TblContent) {
+	rows, err := db.Query(mainTblQuery)
+	if err != nil {
+		content.err = err
+		return
+	}
+
+	// Variables to track information about row content
+	// - these are used to format table columns later
+	var senderIpWidth, targetIpWidth int
+	var snacsSeen bool
+	arpCountHeader := "ARP #"
+	arpCountWidth := len(arpCountHeader)
+	var lastSenderIp string
+
+	defer rows.Close()
+	for rows.Next() {
+
+		//====================
+		// HANDLE DATABASE ROW
+		//====================
+
+		// Variables to hold data retrieved from the db
+		var sender, target eavesarp_ng.Ip
+		var arpCount int
+		var hasSnac bool
+		var senderChanged bool
+
+		// Get data from the sql row
+		err = rows.Scan(&sender.Id, &sender.Value,
+			&target.Id, &target.Value,
+			&arpCount, &hasSnac)
+		if err != nil {
+			content.err = err
+			return
+		}
+
+		// Determine if the SNAC column should be displayed
+		if hasSnac && !snacsSeen {
+			snacsSeen = true
+		}
+
+		//===================
+		// PREPARE ROW VALUES
+		//===================
+
+		// Maximum sender and target IP column widths
+		if len(sender.Value) > senderIpWidth {
+			senderIpWidth = len(sender.Value)
+		}
+		if lastSenderIp != sender.Value {
+			senderChanged = true
+			lastSenderIp = sender.Value
+		}
+		if len(target.Value) > targetIpWidth {
+			targetIpWidth = len(target.Value)
+		}
+
+		// ARP count column width
+		arpCountValue := fmt.Sprintf("%d", arpCount)
+		if len(arpCountValue) > arpCountWidth {
+			arpCountWidth = len(arpCountValue)
+		}
+
+		//======================================
+		// CONSTRUCT AND CAPTURE THE CURRENT ROW
+		//======================================
+
+		var tRow table.Row
+
+		// Include the snacs column if they've been seen regardless
+		// if the row represents a snac
+		if snacsSeen {
+			if hasSnac {
+				tRow = append(tRow, string(emoji.DirectHit))
+			} else {
+				tRow = append(tRow, "")
+			}
+		}
+
+		if senderChanged {
+			tRow = append(tRow, sender.Value)
+		} else {
+			tRow = append(tRow, "")
+		}
+
+		content.rows = append(content.rows, append(tRow, target.Value, arpCountValue))
+	}
+
+	//======================
+	// PREPARE TABLE COLUMNS
+	//======================
+
+	// Include the snacs column if snacs were seen
+	if snacsSeen {
+		content.cols = append(content.cols, table.Column{"", 2})
+	}
+
+	// Add remaining table columns
+	content.cols = append(content.cols,
+		table.Column{"Sender", senderIpWidth},
+		table.Column{"Target", targetIpWidth},
+		table.Column{"ARP #", arpCountWidth})
+
+	return
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
+	case TblContent:
+		if msg.err != nil {
+			// TODO
+			panic(msg.err)
+		}
+		m.table.SetRows(msg.rows)
+		m.table.SetColumns(msg.cols)
+		m.table, cmd = m.table.Update(msg)
+		return m, cmd
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "esc":
@@ -31,6 +173,18 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		case "q", "ctrl+c":
 			return m, tea.Quit
+		case "up", "k":
+			if m.table.Cursor() == 0 {
+				m.table.GotoBottom()
+			} else {
+				m.table.MoveUp(1)
+			}
+		case "down", "j":
+			if m.table.Cursor() == len(m.table.Rows())-1 {
+				m.table.GotoTop()
+			} else {
+				m.table.MoveDown(1)
+			}
 		case "enter":
 			return m, tea.Batch(
 				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
@@ -46,7 +200,7 @@ var baseStyle = lipgloss.NewStyle().
 	BorderForeground(lipgloss.Color("240"))
 
 func (m model) View() string {
-	return baseStyle.Render(m.table.View()) + "\n"
+	return baseStyle.Render(m.table.View()) + "\n" + m.table.HelpView()
 }
 
 func main() {
@@ -57,7 +211,7 @@ func main() {
 	}
 
 	// TODO test the connection by pinging the database
-	db.SetMaxOpenConns(3)
+	//db.SetMaxOpenConns(3)
 	db.SetConnMaxLifetime(0)
 
 	// Apply schema and configurations
@@ -67,52 +221,31 @@ func main() {
 		os.Exit(1)
 	}
 
-	cols := []table.Column{
-		{"IP", 17},
-		{"Discovery Method", 20},
-		{"ARP Resolved", 15},
-		{"PTR Resolved", 15},
+	c := GetTableContent(db)
+	if c.err != nil {
+		// TODO
+		panic(c.err)
 	}
 
-	var tRows []table.Row
-	rows, err := db.Query(`
-SELECT ip.value,ip.disc_meth,ip.arp_resolved,ip.ptr_resolved FROM ip
-
-SELECT * FROM (
-	SELECT sender.id, sender.value, target.id, target.value, (target.arp_resolved=TRUE AND target.mac_id IS NULL) AS SNAC, count FROM arp_count
-	INNER JOIN ip AS sender ON arp_count.sender_ip_id = sender.id
-    INNER JOIN ip AS target ON arp_count.target_ip_id = target.id
-    ORDER BY arp_count.count DESC
-);
-`)
-	if err != nil {
-		panic(err)
-	}
-	for rows.Next() {
-		var ip eavesarp_ng.Ip
-		if err = rows.Scan(&ip.Value, &ip.DiscMethod, &ip.ArpResolved, &ip.PtrResolved); err != nil {
-			panic(err)
-		}
-		tRows = append(tRows, table.Row{ip.Value, string(ip.DiscMethod), fmt.Sprintf("%v", ip.ArpResolved), fmt.Sprintf("%v", ip.PtrResolved)})
-	}
-
-	tbl := table.New(
-		table.WithColumns(cols),
-		table.WithRows(tRows))
 	s := table.DefaultStyles()
 	s.Header = s.Header.
 		BorderStyle(lipgloss.NormalBorder()).
 		BorderForeground(lipgloss.Color("240")).
 		BorderBottom(true).
-		Bold(false)
+		Bold(true).
+		PaddingLeft(1)
+	s.Cell.PaddingLeft(1)
 	s.Selected = s.Selected.
 		Foreground(lipgloss.Color("229")).
 		Background(lipgloss.Color("57")).
 		Bold(false)
-	tbl.SetStyles(s)
 
-	m := model{tbl}
-	if _, err = tea.NewProgram(m).Run(); err != nil {
+	m := model{
+		//table: table.New(table.WithColumns(c.cols), table.WithRows(c.rows), table.WithStyles(s)),
+		table: table.New(table.WithColumns(c.cols), table.WithRows(c.rows), table.WithStyles(s)),
+		db:    db}
+
+	if _, err = tea.NewProgram(m, tea.WithAltScreen()).Run(); err != nil {
 		println("error", err.Error())
 		os.Exit(1)
 	}
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/charmbracelet/x/ansi v0.4.5 // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/enescakir/emoji v1.0.0 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/google/gopacket v1.1.19 // indirect
 	github.com/google/uuid v1.6.0 // indirect
