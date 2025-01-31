9cb27ed302fb0960f4d0aab8dc9862b6fb138004
Justin Angel <justinangel86@gmail.com>
2025-01-31 07:05:06 -0500

    docs

--- cmd/ui/panes/stopwatch.go
+++ cmd/ui/panes/stopwatch.go
@@ -7,12 +7,25 @@ import (
 )
 
 type (
+	// Stopwatch is used to track elapsed time for poisoning attacks
+	// without a time limit. It's adapted from the bubbles module to
+	// suit our need of tracking multiple attacks without excess
+	// messages being sent to the tea control loop.
+	//
+	// It works by capturing a timestamp of each received tick. When
+	// a Stopwatch is stopped and started again, the running duration
+	// is derived by summing it with the time passed since receipt of
+	// the last stop message.
 	Stopwatch struct {
 		id       string
 		d        time.Duration // How long the Stopwatch has been ticking
 		running  bool          // is the Stopwatch running?
-		last     time.Time     // last time ticked time
-		Interval time.Duration // Interval to tick
+		interval time.Duration // interval to tick
+		// Time of last tick.
+		//
+		// When restarted, this value is used to determine how long
+		// the Stopwatch has actually been running.
+		last time.Time
 	}
 	StartStopMsg struct {
 		Id      string
@@ -28,7 +41,7 @@ func NewStopwatch(srcIp, targetIp string, last time.Time, interval time.Duration
 		id:       fmt.Sprintf("%s:%s", srcIp, targetIp),
 		running:  false,
 		last:     last,
-		Interval: interval,
+		interval: interval,
 	}
 }
 
@@ -68,15 +81,15 @@ func (m Stopwatch) Update(msg tea.Msg) (Stopwatch, tea.Cmd) {
 			if !m.last.IsZero() {
 				m.d += time.Since(m.last).Round(time.Second)
 			}
-			cmd = tick(m.id, m.Interval)
+			cmd = tick(m.id, m.interval)
 		}
 	case TickMsg:
 		if !m.running || m.id != msg.Id {
 			break
 		}
 		m.last = time.Now()
-		m.d += m.Interval
-		cmd = tick(m.id, m.Interval)
+		m.d += m.interval
+		cmd = tick(m.id, m.interval)
 	}
 	return m, cmd
 }
