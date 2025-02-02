39d8e99df5e37ea9e7fea24340c975c189bf2e91
Justin Angel <justinangel86@gmail.com>
2025-02-02 09:30:45 -0500

    cleaning

--- cmd/ui/ui.go
+++ cmd/ui/ui.go
@@ -38,6 +38,22 @@ var (
 
 const (
 	CfgPoisonButtonId = "poisonBtn"
+	// TODO we may need to add a limit & offset to this
+	//    unknown how large these queries are going to become
+	convosTableQuery = `
+SELECT sender.id AS sender_ip_id,
+       sender.value AS sender_ip_value,
+       target.id AS target_ip_id,
+       target.value AS target_ip_value,
+       count AS arp_count,
+       (target.arp_resolved AND target.mac_id IS NULL) AS snac
+FROM arp_count
+INNER JOIN ip AS sender ON arp_count.sender_ip_id = sender.id
+INNER JOIN ip AS target ON arp_count.target_ip_id = target.id
+ORDER BY
+	snac DESC,
+    sender.value,
+    arp_count.count DESC`
 )
 
 func init() {
@@ -105,6 +121,12 @@ type (
 	}
 
 	eavesarpError error
+
+	convosTableContent struct {
+		cols []table.Column
+		rows []table.Row
+		err  error
+	}
 )
 
 func newConvoRow(r table.Row) (_ panes.CurConvoRowDetails, err error) {
@@ -645,4 +667,95 @@ func (m *model) handleKeyMsg(msg tea.KeyMsg) (cmd tea.Cmd) {
 	}
 
 	return
+}
+
+func getConvosTableContent(m *model) (content convosTableContent) {
+
+	rows, err := m.db.Query(convosTableQuery)
+	if err != nil {
+		content.err = fmt.Errorf("failed to query conversations content: %w", err)
+		return
+	}
+
+	// Variables to track information about row content
+	// - these are used to format convosTable columns later
+	var senderIpWidth, targetIpWidth int
+	var snacsSeen bool
+	arpCountHeader := "ARP #"
+	arpCountWidth := len(arpCountHeader)
+
+	defer rows.Close()
+
+	for rowInd := 1; rows.Next(); rowInd++ {
+
+		//====================
+		// HANDLE DATABASE ROW
+		//====================
+
+		// Variables to hold data retrieved from the db
+		var sender, target eavesarp_ng.Ip
+		var arpCount int
+		var hasSnac bool
+		//var senderChanged bool
+
+		// Get data from the sql row
+		err = rows.Scan(&sender.Id, &sender.Value,
+			&target.Id, &target.Value,
+			&arpCount, &hasSnac)
+		if err != nil {
+			content.err = fmt.Errorf("failed to scan conversations row: %w", err)
+			return
+		}
+
+		var senderPoisoned string
+		if is := activeAttacks.Exists(eavesarp_ng.FmtConvoKey(sender.Value, target.Value)); is {
+			senderPoisoned = m.senderPoisonedChar
+		}
+
+		// Determine if the SNAC column should be displayed
+		if hasSnac && !snacsSeen {
+			snacsSeen = true
+		}
+
+		arpCountValue := fmt.Sprintf("%d", arpCount)
+
+		//=====================
+		// ADJUST WIDTH OFFSETS
+		//=====================
+
+		eavesarp_ng.GreaterLength(sender.Value, &senderIpWidth)
+		eavesarp_ng.GreaterLength(target.Value, &targetIpWidth)
+		eavesarp_ng.GreaterLength(arpCountValue, &arpCountWidth)
+
+		//======================================
+		// CONSTRUCT AND CAPTURE THE CURRENT ROW
+		//======================================
+
+		tRow := table.Row{fmt.Sprintf("%d", rowInd)}
+
+		if hasSnac {
+			tRow = append(tRow, m.snacChar, senderPoisoned)
+		} else {
+			tRow = append(tRow, "", senderPoisoned)
+		}
+
+		tRow = append(tRow, sender.Value)
+		content.rows = append(content.rows, append(tRow, target.Value, arpCountValue))
+	}
+
+	//======================
+	// PREPARE TABLE COLUMNS
+	//======================
+
+	// Include the snacs column if snacs were seen
+	content.cols = append(content.cols,
+		table.Column{"#", len(fmt.Sprintf("%d", len(content.rows)))},
+		table.Column{"SNAC", 4},
+		table.Column{"Poisoned", 8},
+		table.Column{"Sender", senderIpWidth},
+		table.Column{"Target", targetIpWidth},
+		table.Column{arpCountHeader, arpCountWidth})
+
+	return
+
 }
